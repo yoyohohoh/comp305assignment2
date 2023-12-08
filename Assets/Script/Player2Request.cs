321a0481b08feab53c06e3da1a493c534a85d3fd
yoyohohoh <yho45@my.centennialcollege.ca>
2023-12-08 13:37:44 -0500

    level 1 player 2

--- Assets/Script/Player2Request.cs
+++ Assets/Script/Player2Request.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player2Request : MonoBehaviour
 {
     public GameObject player2;
+    public Button button;
 
     public void RequestPlayer()
     {
-        Debug.Log("Request Player 2");
-        player2.SetActive(true);
+
+        //if there is no player 2 in the game
+        if (GameObject.Find("Player2") == null)
+        {
+            Debug.Log("Request Player 2");
+            player2.SetActive(true);
+            button.GetComponent<Image>().color = new Color(200 / 255f, 200 / 255f, 200 / 255f);
+        }
+        else
+        {
+            Debug.Log("Player 2 is already in the game");
+        }
     }
 }
